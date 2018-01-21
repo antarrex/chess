061b893eedbab0a1afff57898c52473fc68ab6bd
maxim-larkin <malmsteen997@hotmail.com>
2018-01-21 20:10:58 +0300

    refactoring

--- app/App.php
+++ app/App.php
@@ -1,7 +1,66 @@
 <?php
 namespace App;
 
+use App\Board\Board;
+
 class App
 {
+    private $board;
 
+    public function __construct(Board $board)
+    {
+        $this->board = $board;
+    }
+
+    public function handleInput($input)
+    {
+        if (in_array('add', $input)) {
+            $this->board->add($input[1], $input[2]);
+        }
+
+        if (in_array('remove', $input)) {
+            $this->board->remove($input[1]);
+        }
+
+        if (in_array('move', $input)) {
+            $this->board->move($input[1], $input[2]);
+        }
+
+        if (in_array('save', $input)) {
+            $this->board->save();
+        }
+
+        if (in_array('load', $input)) {
+            $this->board->load();
+        }
+
+        $this->board->printState();
+    }
+
+    public function showWelcomeMessage()
+    {
+        echo "
+        Welcome to console chess board!
+        Allowed piece names:
+            'king',
+            'queen',
+            'rook',
+            'bishop',
+            'knight',
+            'pawn'
+        Available commands:
+        add <piece> <cell>
+        example - 'add bishop A1'
+
+        remove <cell>'
+        example - 'remove A1'
+
+        move <cell_from> <cell_to>
+        example - 'move A1 C3'
+
+        save - saves current board state
+        load - load last saved state
+        press CTRL+Z to end the game
+        " . PHP_EOL;
+    }
 }
--- chess.php
+++ chess.php
@@ -1,61 +1,17 @@
 <?php
 require 'vendor/autoload.php';
 
+use App\App;
 use App\Board\Board;
 use \App\Storage\FileStorage;
-echo "
-Welcome to console chess board!
-Allowed piece names:
-    'king',
-    'queen',
-    'rook',
-    'bishop',
-    'knight',
-    'pawn'
-Available commands:
-add <piece> <cell>
-example - 'add bishop A1'
-
-remove <cell>'
-example - 'remove A1'
-
-move <cell_from> <cell_to>
-example - 'move A1 C3'
-
-save - saves current board state
-load - load last saved state
-type 'exit' to end the game
-" . PHP_EOL;
 
 $board = new Board(new FileStorage);
+$app = new App($board);
+
+$app->showWelcomeMessage();
 
 while (true) {
-    $board->printState();
-
     $input = explode(' ', trim(fgets(STDIN, 1024)));
 
-    if (in_array('add', $input)) {
-        $board->add($input[1], $input[2]);
-    }
-
-    if (in_array('remove', $input)) {
-        $board->remove($input[1]);
-    }
-
-    if (in_array('move', $input)) {
-        $board->move($input[1], $input[2]);
-    }
-
-    if (in_array('save', $input)) {
-        $board->save();
-    }
-
-    if (in_array('load', $input)) {
-        $board->load();
-    }
-
-    if (in_array('exit', $input)) {
-        print 'Bye!' . PHP_EOL;
-        break;
-    }
+    $app->handleInput($input);
 }
